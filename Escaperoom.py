9a53ad5b5177fa5a6354b7469a9b43befc090907
Jokkko <joaquinfernandes00@gmail.com>
2024-08-20 09:48:40 -0300

    Finalizado movimiento del jugador, interactuar WIP

--- Escaperoom.py
+++ Escaperoom.py
@@ -1,6 +1,12 @@
 import random
 
+vaciarConsola = "\n" * 50
+
 def GenerarMapa(alturaMin,alturaMax,anchoMin,anchoMax):
+    '''
+    Genera un mapa de juego con una altura y un ancho, minimo y maximo
+    Ademas de rellenar el mapa con un terreno, un personaje, almenos un candado y almenos una pista.
+    '''
     mapa = []
     terreno = "/"
     personaje = "O"
@@ -13,6 +19,10 @@ def GenerarMapa(alturaMin,alturaMax,anchoMin,anchoMax):
     return mapa
 
 def GenerarTereno(mapa,alturaMin,alturaMax,anchoMin,anchoMax,terreno):
+    '''
+    Genera el terreno de un mapa vacio pasado por parametro, el tipo de terreno sera definido por parametro
+    el ancho y la altura sera un numero entre los minimos y maximos pasados por parametros.
+    '''
     altoDelMapa = random.randint(alturaMin,alturaMax)
     anchoMinimoDelMapa = random.randint(anchoMin,anchoMax) 
 
@@ -43,14 +53,37 @@ def LeerAccion():
         accion = input("Elija una accion ('W','A','S','D' para el movimiento. 'I' para interactuar): ")
     return accion
 
+def GetIndiceObjeto(mapa,objeto):
+    indices = []
+    for fila in range(len(mapa)):
+        for columna in range(len(mapa[fila])):
+            if(mapa[fila][columna] == objeto):
+                indices.append(fila)
+                indices.append(columna)
+    return indices
 
-#def AccionPersonaje(accion):
-#    if(accion == "w"):
-        
+def AccionPersonaje(mapa,accion):
+    terreno = "/"
+    personaje = "O"
+    
+    if(accion == "w"):
+        MoverObjeto(mapa,0,1,personaje)
+    elif(accion == "s"):
+        MoverObjeto(mapa,0,-1,personaje) 
+    elif(accion == "a"):
+        MoverObjeto(mapa,-1,0,personaje)
+    elif(accion == "d"):
+        MoverObjeto(mapa,1,0,personaje)
 
+def MoverObjeto(mapa,x,y,objeto):
+    terreno = "/"
+    coordenadasObjeto = GetIndiceObjeto(mapa,objeto)
+    mapa[coordenadasObjeto[0]][coordenadasObjeto[1]] = terreno
+    mapa[coordenadasObjeto[0]+y*(-1)][coordenadasObjeto[1]+x] = objeto
 
 
 mapa = GenerarMapa(6,11,8,16)
-
-RenderizarMapa(mapa)
-#AccionPersonaje(LeerAccion())
+while(True): #PARA TESTEAR
+    print(vaciarConsola)
+    RenderizarMapa(mapa)
+    AccionPersonaje(mapa,LeerAccion())
