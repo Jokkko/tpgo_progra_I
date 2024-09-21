74318397f45112aeb5cbd72ea872ec72f7e6fe16
Jokkko <joaquinfernandes00@gmail.com>
2024-09-21 11:38:58 -0300

    Pedir user Finalizado, queda que le agreguen la validacion de que no existe el user

--- Escaperoom.py
+++ Escaperoom.py
@@ -2,24 +2,9 @@ import random
 import re
 
 terreno = " "
+userRepository = []
 
-def GenerarMapa(alturaMin,alturaMax,anchoMin,anchoMax):
-    '''
-    Genera un mapa de juego con una altura y un ancho, minimo y maximo
-    Ademas de rellenar el mapa con un terreno, un personaje, almenos un candado y almenos una pista.
-    '''
-    mapa = []
-    #terreno = " "
-    personaje = "O"
-    candados = "$"
-    pistas = "#"
-    GenerarTereno(mapa,alturaMin,alturaMax,anchoMin,anchoMax,terreno)
-    GenerarObjeto(mapa,personaje,1,terreno)
-    GenerarObjeto(mapa,candados,2,terreno)
-    GenerarObjeto(mapa,pistas,2,terreno)
-    return mapa
-
-def GenerarTereno(mapa,alturaMin,alturaMax,anchoMin,anchoMax,terreno):
+def GenerarTereno(mapa,alturaMin,alturaMax,anchoMin,anchoMax):
     '''
     Genera el terreno de un mapa vacio pasado por parametro, el tipo de terreno sera definido por parametro
     el ancho y la altura sera un numero entre los minimos y maximos pasados por parametros.
@@ -33,7 +18,7 @@ def GenerarTereno(mapa,alturaMin,alturaMax,anchoMin,anchoMax,terreno):
             fila.append(terreno) 
         mapa.append(fila)
 
-def GenerarObjeto(mapa,objeto,cantidad,terreno):
+def GenerarObjeto(mapa,objeto,cantidad):
     columnaSpawn = random.randint(0,len(mapa)-1) 
     filaSpawn = random.randint(0,len(mapa[columnaSpawn])-1)
     for _ in range(cantidad):
@@ -41,6 +26,28 @@ def GenerarObjeto(mapa,objeto,cantidad,terreno):
             columnaSpawn = random.randint(0,len(mapa)-1) 
             filaSpawn = random.randint(0,len(mapa[columnaSpawn])-1)
         mapa[columnaSpawn][filaSpawn] = objeto
+
+def GenerarMapa(alturaMin,alturaMax,anchoMin,anchoMax):
+    '''
+    Genera un mapa de juego con una altura y un ancho, minimo y maximo
+    Ademas de rellenar el mapa con un terreno, un personaje, almenos un candado y almenos una pista.
+    '''
+    mapa = []
+    #terreno = " "
+    personaje = "O"
+    candados = "$"
+    pistas = "#"
+    objetos = [personaje,candados,pistas]
+    GenerarTereno(mapa,alturaMin,alturaMax,anchoMin,anchoMax)
+    for i in objetos:
+        if(i == personaje):
+            GenerarObjeto(mapa,i,1)
+        else:
+            GenerarObjeto(mapa,i,2) 
+        
+    return mapa
+
+
 
 def RenderizarMapa(mapa):
     for fila in mapa:
@@ -93,9 +100,20 @@ def PedirUserName():
     nombreValido = re.match(patron,username)
     while(nombreValido == None):
         print("Nombre no valido. Intentelo denuevo")    
-        username = input("Porfavor Ingrese su nombre de usuario, \nno puede contener mas de 10 caracteres ni menos de 3, numeros, ni caracteres especiales:")
+        username = input("Porfavor Ingrese su nombre de usuario, \nNo puede contener mas de 10 caracteres ni menos de 3, numeros, ni caracteres especiales:")
         nombreValido = re.match(patron,username)
+    GenerarUser(username)
     return username
+
+def GenerarUser(username):
+    userRepository.append(dict(Username=username,Id = GenerarId() ))
+
+def GenerarId():
+    id = 0
+    if(len(userRepository) != 0):
+        ultimo = len(userRepository)-1 
+        userRepository[ultimo]['Id'] + 1
+    return id
 
 def PedirOpcion(min,max):
     #pida una opcion(numero) al usuario y la devuelva
@@ -158,24 +176,27 @@ def main():
     ##PARA TESTEAR MOVIMIENTO
     mapa = GenerarMapa(6,11,8,16)
     PedirUserName()
-    while(True): 
-        print(vaciarConsola)
-        RenderizarMapa(mapa)
-        AccionPersonaje(mapa,LeerAccion())
-    #jugando = True
-    #dificultad = 0
-    #tematica = 0
-    #while(jugando):
-    #    MenuPrincipal()
-    #    opcion = PedirOpcion()
-    #    if(opcion == 1):
-    #        dificultad = SeleccionarDificultad()
-    #        tematica = ElegirTematica(dificultad)
-    #        ComenzarJuego(tematica)
-    #    elif (opcion == 0):
-    #        print("Saliendo...")
-    #        jugando = False
+    #while(True): 
+    #    print(vaciarConsola)
+    #    RenderizarMapa(mapa)
+    #    AccionPersonaje(mapa,LeerAccion())
+    jugando = True
+    dificultad = 0
+    tematica = 0
+    while(jugando):
+        MenuPrincipal()
+        opcion = PedirOpcion()
+        if(opcion == 1):
+            dificultad = SeleccionarDificultad()
+            tematica = ElegirTematica(dificultad)
+            ComenzarJuego(tematica)
+        elif (opcion == 0):
+            print("Saliendo...")
+            jugando = False
     
 
-
-main()
+mapa = GenerarMapa(6,11,8,16)
+while(True): 
+    print(vaciarConsola)
+    RenderizarMapa(mapa)
+    AccionPersonaje(mapa,LeerAccion())
